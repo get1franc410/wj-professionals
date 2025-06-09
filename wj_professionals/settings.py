09386168ed20496fe93925e43bb5ef14fad34df8
Adeyanju Joshua <omowolewa@gmail.com>
2025-06-09 19:53:16 +0100

    Prepare for production deployment

--- wj_professionals/settings.py
+++ wj_professionals/settings.py
@@ -16,16 +16,22 @@ import os
 from pathlib import Path
 from decouple import config
 
+# Use environment variables
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY', default='your-fallback-secret-key')
+DEBUG = config('DEBUG', default=True, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me')
+# SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+# DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.wjprofessionals.com']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.wjprofessionals.com']
 
 # Application definition
 DJANGO_APPS = [
